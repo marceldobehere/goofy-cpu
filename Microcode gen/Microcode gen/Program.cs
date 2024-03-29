5468c1c839392e0e6ff3a23e905a6605f78f92fb
Masl <marcel.z2006@gmail.com>
2024-03-29 23:57:42 +0100

    a

--- Microcode gen/Microcode gen/Program.cs
+++ Microcode gen/Microcode gen/Program.cs
@@ -4,36 +4,37 @@
     {
         public string Name;
         public byte Opcode;
-        public List<long> Steps;
-        public Instruction(string name, byte opcode, List<long> steps)
+        public List<ulong> Steps;
+        public Instruction(string name, byte opcode, List<ulong> steps)
         {
             this.Name = name;
             this.Opcode = opcode;
             this.Steps = steps;
 
-            if (Steps.Count > 0 && Steps[Steps.Count - 1] != DONE)
+            if (Steps.Count == 0 || Steps[Steps.Count - 1] != DONE)
                 Steps.Add(DONE);
         }
     }
 
-    public const long DONE = 0b0000_0000_0000_0000;
+    public const ulong DONE = (ulong)1 << 63;
 
-    public const long PUT_REG_IOP0_LO_BUS = 0b0000_0000_0000_0001;
-    public const long PUT_REG_IOP0_HI_BUS = 0b0000_0000_0000_0010;
+    public const ulong PUT_REG_IOP0_LO_BUS = 1 << 0;
+    public const ulong PUT_REG_IOP0_HI_BUS = 1 << 1;
 
-    public const long STR_REG_IOP0_LO_BUS = 0b0000_0000_0000_0011;
-    public const long STR_REG_IOP0_HI_BUS = 0b0000_0000_0000_0100;
+    public const ulong STR_REG_IOP0_LO_BUS = 1 << 2;
+    public const ulong STR_REG_IOP0_HI_BUS = 1 << 3;
 
-    public const long PUT_VAL_IOP0_BUS = 0b0000_0000_0000_0101;
-    public const long PUT_VAL_IOP1_BUS = 0b0000_0000_0000_0110;
+    public const ulong PUT_VAL_IOP0_BUS = 1 << 4;
+    public const ulong PUT_VAL_IOP1_BUS = 1 << 5;
 
-    public const long STR_REG_0__BUS = 0b0000_0000_0000_0111;
-    public const long STR_REG_1__BUS = 0b0000_0000_0000_1000;
-    public const long STR_REG_2__BUS = 0b0000_0000_0000_1001;
-    public const long STR_REG_3__BUS = 0b0000_0000_0000_1010;
+    public const ulong STR_REG_0_BUS = 1 << 6;
+    public const ulong STR_REG_1_BUS = 1 << 7;
+    public const ulong STR_REG_2_BUS = 1 << 8;
+    public const ulong STR_REG_3_BUS = 1 << 9;
 
     public static void Main(string[] args)
     {
+        Console.WriteLine(DONE);
         List<Instruction> instructions = new List<Instruction>();
 
 
@@ -42,30 +43,30 @@
         //> MOVE
         //  0000.0000 REG1.REG2 0000.0000 (REG2 -> REG1)
         // REG1 IS HI, REG2 IS LO
-        instructions.Add(new Instruction("MOVE", 0b0000_0000, new List<long> {
+        instructions.Add(new Instruction("MOVE", 0b0000_0000, new List<ulong> {
             PUT_REG_IOP0_LO_BUS | STR_REG_IOP0_HI_BUS
         }));
 
         //> MOVEI
         //  0000.0001 REG1.0000 AAAA.AAAA (AAAA -> REG1)
         // REG1 IS HI
-        instructions.Add(new Instruction("MOVEI", 0b0000_0001, new List<long> {
+        instructions.Add(new Instruction("MOVEI", 0b0000_0001, new List<ulong> {
             PUT_VAL_IOP1_BUS | STR_REG_IOP0_HI_BUS
         }));
 
         //> LDA
         //  0000.0010 AAAA.AAAA AAAA.AAAA (AAAA -> REGA)
-        instructions.Add(new Instruction("LDA", 0b0000_0010, new List<long> {
-            PUT_VAL_IOP0_BUS | STR_REG_0__BUS,
-            PUT_VAL_IOP1_BUS | STR_REG_1__BUS
+        instructions.Add(new Instruction("LDA", 0b0000_0010, new List<ulong> {
+            PUT_VAL_IOP0_BUS | STR_REG_0_BUS,
+            PUT_VAL_IOP1_BUS | STR_REG_1_BUS
         }));
 
         //> LDB
         //  0000.0011 AAAA.AAAA AAAA.AAAA (AAAA -> REGB)
-        instructions.Add(new Instruction("LDB", 0b0000_0011, new List<long>
+        instructions.Add(new Instruction("LDB", 0b0000_0011, new List<ulong>
         {
-            PUT_VAL_IOP0_BUS | STR_REG_2__BUS,
-            PUT_VAL_IOP1_BUS | STR_REG_3__BUS
+            PUT_VAL_IOP0_BUS | STR_REG_2_BUS,
+            PUT_VAL_IOP1_BUS | STR_REG_3_BUS
         }));
 
 
@@ -78,17 +79,82 @@
 
         // List Instructions
         Console.WriteLine("Instructions: ");
-        foreach (var instruction in instructions)
+        foreach (var inst in instructions)
         {
-            Console.WriteLine($"{instruction.Name} - {instruction.Opcode}");
-            foreach (var step in instruction.Steps)
-                Console.WriteLine($"  {Convert.ToString(step, 2).PadLeft(16, '0')}");
+            Console.WriteLine($" > {inst.Name} ({inst.Opcode}) ({inst.Steps.Count} Steps):");
+            foreach (ulong step in inst.Steps)
+                Console.WriteLine($"   - {Convert.ToString((long)step, 16).PadLeft(16, '0')}");
             Console.WriteLine(  );
         }
 
+
+        ExportMicrocode("microcode.hex", instructions);
+    }
+
+    public static ulong[,] ConvertMicrocode(List<Instruction> instructions)
+    {
+        // THE ROM HAS A 12 BIT ADDRESS
+        // HI 8 BITS ARE THE OPCODE
+        // LO 4 BITS ARE THE STEP INDEX
+        // PER ADRESS IT STORES A 32 BIT "STEP" VALUE
+        ulong[,] data = new ulong[256, 16];
+        foreach (var inst in instructions)
+        {
+            if (inst.Steps.Count > 16)
+                throw new Exception("Instruction has too many steps!");
+
+            for (int i = 0; i < inst.Steps.Count; i++)
+                data[inst.Opcode, i] = (ulong)inst.Steps[i];
+        }
+
+        return data;
+    }
+
+    public static void ExportMicrocode(string name, List<Instruction> instructions)
+    {
+        ExportData(name, ConvertMicrocode(instructions));
+    }
+
+    public static string DataToString(ulong[,] data, int yIndex, int xStart)
+    {
+        string line = "";
+        for (int x = xStart; x < xStart + 8; x++)
+            line += Convert.ToString((long)data[yIndex, x], 16).PadLeft(16, '0') + " ";
+        return line;
+    }
+
+    public static void ExportData(string name, ulong[,] data)
+    {
+        // Header
+        // Then 8 steps per line
+        // -> 2 lines per instruction
+
+        using (StreamWriter sw = new StreamWriter(name))
+        {
+            sw.WriteLine("v3.0 hex words plain");
+
+            for (int ins = 0; ins < data.GetLength(0); ins++)
+            {
+                sw.WriteLine(DataToString(data, ins, 0));
+                sw.WriteLine(DataToString(data, ins, 8));
+            }
+        }
     }
 }
 
+/* EXAMPLE OF OUTPUT FILE
+v3.0 hex words plain
+00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000
+00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000
+00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000
+00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000
+00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000
+00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000
+00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000
+00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000
+00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000
+00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000
+*/
 
 
 /*
